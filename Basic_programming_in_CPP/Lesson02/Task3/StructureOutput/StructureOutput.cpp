cee849a29b9e46b2d3111b034f70f881765d9a63
Loguntsov Maxim <loguncovmaxim@gmail.com>
2023-01-24 15:49:35 +0300

    Lesson2Task3

---
+++ Basic_programming_in_CPP/Lesson02/Task3/StructureOutput/StructureOutput.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <windows.h>
+
+	//Структура адреса
+struct address {
+	std::string country = "Россия";
+	std::string town = "Москва";
+	std::string street = "Ленина";
+	int houseNumber = 4;
+	int apartmentNumber = 34;
+	int index = 342567;
+};
+
+void outputStruct(address& addr) {
+	
+	//Вывод структуры
+	std::cout << "Страна: " << addr.country << std::endl;
+	std::cout << "Город: " << addr.town << std::endl;
+	std::cout << "Улица: " << addr.street << std::endl;
+	std::cout << "Номер дома: " << addr.houseNumber << std::endl;
+	std::cout << "Номер квартиры: " << addr.apartmentNumber << std::endl;
+	std::cout << "Индекс: " << addr.index << std::endl;
+}
+
+int main() {
+	
+	//Руссификатор
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	address addr;
+	outputStruct(addr);
+}
