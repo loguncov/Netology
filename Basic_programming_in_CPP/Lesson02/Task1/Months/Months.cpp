3731b0518da3f0b85513135b0b3fb7530cb78653
Loguntsov Maxim <loguncovmaxim@gmail.com>
2023-01-24 14:57:55 +0300

    Lesson2Task1

---
+++ Basic_programming_in_CPP/Lesson02/Task1/Months/Months.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+enum class Month : uint8_t {
+    Incorrect_Month,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+const char* month_str[] = { "Неизвестный месяц",
+                            "Январь",
+                            "Февраль",
+                            "Март",
+                            "Апрель",
+                            "Май",
+                            "Июнь",
+                            "Июль",
+                            "Август",
+                            "Сентябрь",
+                            "Октябрь",
+                            "Ноябрь",
+                            "Декабрь" };
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите номер месяца : ";
+    while (true) {
+        unsigned month;
+        std::cin >> month;
+
+        if (month > static_cast<int>(Month::December)) {
+            std::cout << "Неправильный номер!" << std::endl;
+            continue;
+        }
+
+        if (month == static_cast<int>(Month::Incorrect_Month)) {
+            std::cout << "До свидания" << std::endl;
+            break;
+        }
+
+        std::cout << month_str[static_cast<int>(month)] << std::endl;
+    }
+}
