4e6a050ce643baaab42051b927a5c87a55e5f9ab
loguncov <loguncovmaxim@gmail.com>
2023-04-24 10:55:48 +0300

    3.2

--- Advanced_programming _in_CPP/Lesson03/Task2/Copying_Smart_Arrays/copying_smart_arrays.cpp
+++ Advanced_programming _in_CPP/Lesson03/Task2/Copying_Smart_Arrays/copying_smart_arrays.cpp
@@ -1,90 +1,86 @@
 #include <iostream>
-#include <exception>
-class smart_array {
-private:
-int* data;
-int size;
-public:
-// конструктор с одним параметром - размер массива
-smart_array(int s) {
-size = s;
-data = new int[size];
-}
-// конструктор копирования
-smart_array(const smart_array& other) {
-    size = other.size;
-    data = new int[size];
-    for (int i = 0; i < size; i++) {
-        data[i] = other.data[i];
-    }
-}
 
-// оператор присваивания
-smart_array& operator=(const smart_array& other) {
-    if (this == &other) {
+class Smart_Array
+{
+private:
+    int amount = 0; // размер выделенной памяти для хранения массива
+    int logical_size = 0; // текущий размер массива
+    int* smart_array; // указатель на выделенную память для массива
+
+public:
+    // конструктор принимает размер массива
+    Smart_Array(int sum) : amount(sum)
+    {
+        smart_array = new int[amount]; // выделение памяти под массив
+    }
+
+    // конструктор копирования
+    Smart_Array(const Smart_Array& other)
+    {
+        amount = other.amount; // копируем размер выделенной памяти
+        logical_size = other.logical_size; // копируем размер текущего массива
+
+        smart_array = new int[amount](); // выделяем память для копии массива и заполняем нулями
+
+        // копируем элементы из оригинального массива в копию
+        for (int i = 0; i < logical_size; i++)
+        {
+            smart_array[i] = other.smart_array[i];
+        }
+    }
+
+    // деструктор освобождает выделенную память
+    ~Smart_Array()
+    {
+        delete[] smart_array;
+    }
+
+    // добавление элемента в массив
+    void add_element(int element)
+    {
+        smart_array[logical_size] = element;
+        logical_size += 1;
+    }
+
+    // получение элемента массива по индексу
+    int get_element(int index)
+    {
+        int element = smart_array[index];
+        return element;
+    }
+
+    // оператор присваивания
+    Smart_Array operator=(const Smart_Array& other)
+    {
+        logical_size = other.logical_size; // копируем размер текущего массива
+        amount = other.amount; // копируем размер выделенной памяти
+        delete[] smart_array; // удаляем старую выделенную память
+        smart_array = new int[amount]; // выделяем новую память
+        for (int i = 0; i < logical_size; ++i)
+        {
+            smart_array[i] = other.smart_array[i]; // копируем элементы из оригинального массива в новый
+        }
+
         return *this;
     }
-    delete[] data;
-    size = other.size;
-    data = new int[size];
-    for (int i = 0; i < size; i++) {
-        data[i] = other.data[i];
-    }
-    return *this;
-}
 
-// деструктор
-~smart_array() {
-    delete[] data;
-}
-
-// функция добавления элемента в массив
-void add_element(int element) {
-    if (size == 0) {
-        data = new int[1];
-        size = 1;
-        data[0] = element;
-    }
-    else if (size > 0) {
-        int* new_data = new int[size + 1];
-        for (int i = 0; i < size; i++) {
-            new_data[i] = data[i];
-        }
-        new_data[size] = element;
-        size++;
-        delete[] data;
-        data = new_data;
-    }
-}
-
-// функция получения элемента по индексу
-int get_element(int index) const {
-    if (index >= size || index < 0) {
-        throw std::out_of_range("Index out of range");
-    }
-    return data[index];
-}
 };
 
-// пример использования
-int main() {
-try {
-smart_array arr(5);
-arr.add_element(1);
-arr.add_element(4);
-arr.add_element(155);
-    smart_array new_array(2);
-    new_array.add_element(44); 
-    new_array.add_element(34);
+int main()
+{
+    Smart_Array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
 
-    arr = new_array;
+    Smart_Array newArr(2);
+    newArr.add_element(44);
+    newArr.add_element(34);
+
+    arr = newArr; // присваиваем массиву arr значения массива newArr
 
     std::cout << arr.get_element(0) << std::endl; // 44
     std::cout << arr.get_element(1) << std::endl; // 34
-    std::cout << arr.get_element(2) << std::endl; // Index out of range
-}
-catch (const std::exception& ex) {
-    std::cout << ex.what() << std::endl;
-}
-return 0;
+    std::cout << arr.get_element(2) << std::endl; // Индекс вне диапазона
+
 }
