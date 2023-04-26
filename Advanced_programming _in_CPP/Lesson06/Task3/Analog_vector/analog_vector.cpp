4fd0c0094fbaf1ab1edebd41d56a1241100422cf
Maxim Loguntsov <loguncovmaxim@gmail.com>
2023-04-26 13:47:21 +0300

    Update analog_vector.cpp

--- Advanced_programming _in_CPP/Lesson06/Task3/Analog_vector/analog_vector.cpp
+++ Advanced_programming _in_CPP/Lesson06/Task3/Analog_vector/analog_vector.cpp
@@ -17,6 +17,38 @@ public:
             delete[] m_data;
         }
     }
+    
+    // конструктор копирования
+MyVector(const MyVector& other) : m_data(nullptr), m_size(0), m_capacity(0) {
+    // копируем данные из other
+    m_size = other.m_size;
+    m_capacity = other.m_capacity;
+    m_data = new T[m_capacity];
+    for (size_t i = 0; i < m_size; ++i) {
+        m_data[i] = other.m_data[i];
+    }
+}
+
+// оператор присваивания
+MyVector& operator=(const MyVector& other) {
+    if (this != &other) {
+        // освобождаем текущую память
+        delete[] m_data;
+        m_data = nullptr;
+        m_size = 0;
+        m_capacity = 0;
+
+        // копируем данные из other
+        m_size = other.m_size;
+        m_capacity = other.m_capacity;
+        m_data = new T[m_capacity];
+        for (size_t i = 0; i < m_size; ++i) {
+            m_data[i] = other.m_data[i];
+        }
+    }
+    return *this;
+}
+
 
     // функция доступа к элементу по индексу
     T& at(size_t index) {
@@ -67,14 +99,24 @@ public:
 
 int main() {
     // создаем контейнер и добавляем элементы
-    MyVector<int> vec;
-    vec.push_back(10);
-    vec.push_back(20);
-    vec.push_back(30);
+    MyVector<int> vec1;
+    vec1.push_back(10);
+    vec1.push_back(20);
+    vec1.push_back(30);
 
     // выводим элементы на экран
-    for (size_t i = 0; i < vec.size(); ++i) {
-        std::cout << vec.at(i) << " ";
+    for (size_t i = 0; i < vec1.size(); ++i) {
+        std::cout << vec1.at(i) << " ";
+    }
+    std::cout << std::endl;
+
+    // создаем второй контейнер и присваиваем ему значения первого контейнера
+    MyVector<int> vec2;
+    vec2 = vec1;
+
+    // выводим элементы второго контейнера на экран
+    for (size_t i = 0; i < vec2.size(); ++i) {
+        std::cout << vec2.at(i) << " ";
     }
     std::cout << std::endl;
 
